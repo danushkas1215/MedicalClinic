7a7b222138f636e90407d59f1731a649c0aed69e
danushkas1215 <danushkas@gmail.com>
2017-07-25 15:14:07 +0530

    Database modified

--- MedicalClinic/PharmaceuticalCom.Designer.cs
+++ MedicalClinic/PharmaceuticalCom.Designer.cs
@@ -36,6 +36,8 @@
             this.label3 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -103,11 +105,32 @@
             this.label4.TabIndex = 6;
             this.label4.Text = "Company Email";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(314, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(439, 313);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // PharmaceuticalCom
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(828, 514);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox3);
@@ -133,5 +156,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- MedicalClinic/PharmaceuticalCom.cs
+++ MedicalClinic/PharmaceuticalCom.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace MedicalClinic
 {
@@ -15,6 +16,18 @@ namespace MedicalClinic
         public PharmaceuticalCom()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OleDbConnection con = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\dsi.SL\\Documents\\Visual Studio 2017\\Projects\\MedicalClinic\\MedicalClinic\\MedicalClinic.accdb");
+            OleDbCommand cmd = con.CreateCommand();
+            con.Open();
+            cmd.CommandText = "Insert into PharmaceuticalCompanies(CompanyName,CompanyAddress)Values('" + textBox1.Text + "','" + textBox2.Text + "')";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Record Submitted", "Congrats");
+            con.Close();
         }
     }
 }
